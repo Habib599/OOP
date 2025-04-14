b3ff546675d8acd433176914e70d26dd9eb386c9
Md Habibur Rahman <mdhabiburr599@gmail.com>
2025-04-14 11:00:08 +0600

    hi

---
+++ 01_module/first.py
@@ -0,0 +1,2 @@
+print('kopa python kopa')
+print('python dore felsi mama!!!')
---
+++ 04_module  assignment 1/py autogui.py
@@ -0,0 +1,7 @@
+import pyautogui
+from time import sleep
+# sleep(5)
+for i in range(0,100):
+    pyautogui.write('fuck you')
+    pyautogui.press('enter')
+
